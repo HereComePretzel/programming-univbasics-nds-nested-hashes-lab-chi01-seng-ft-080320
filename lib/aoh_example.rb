9dc82309297eec0a7740f2e43aeed6e05f7d31b5
Austin Oie <austinoie@gmail.com>
2020-06-29 14:06:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,6 +18,7 @@ def literal_aoh
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     
+    
     ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
